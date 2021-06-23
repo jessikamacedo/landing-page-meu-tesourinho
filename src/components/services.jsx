462bd4c26d1255e09c3dcfd57b3bf88c55c6ff73
Jessika <jessika.msmacedo@gmail.com>
2021-06-22 21:49:03 -0300

    upload homepage

---
+++ src/components/services.jsx
@@ -0,0 +1,28 @@
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Como Usar?</h2>
+          <p>
+            Conheça um pouco mais sobre como usar cada pedacinho da nossa plataforma.
+          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  {' '}
+                  <i className={d.icon}></i>
+                  <div className='service-desc'>
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
